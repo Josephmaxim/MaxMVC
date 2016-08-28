c40ab77ecd85f2df9cf8f34bc88006d6c040ead0
Joseph Maxim <JosephMaxim@Josephs-MacBook-Pro.local>
2016-08-27 23:39:32 -0400

    Change local css to CDNs

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -1,6 +1,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="<?php echo $GLOBALS['config']['jsUrl'];?>bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <!-- Magic Spells -->
 <script>
--- view/includes/header.php
+++ view/includes/header.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <title>Joseph Maxim Dagunan</title>
 
-    <link href="<?php echo $GLOBALS['config']['cssUrl'];?>bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo $GLOBALS['config']['cssUrl'];?>font-awesome.min.css" rel="stylesheet">
-    <link href="<?php echo $GLOBALS['config']['cssUrl'];?>animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
     <link href="<?php echo $GLOBALS['config']['cssUrl'];?>main.css" rel="stylesheet">
 
     <!--[if lt IE 9]>
